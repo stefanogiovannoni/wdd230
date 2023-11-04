141bcf6990566378888145776dc9356d36d3c936
Stefano Giovanoni <stefanogiovannonii@gmail.com>
2023-11-04 02:17:13 -0300

    Course Home Page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,9 +1,10 @@
-let oLastModif = new Date(document.lastModified);
-const options = {
-    weekday: "long",
-    day: "numeric",
-    month:"long",
-    year:"numeric",
-  };
+const year = document.querySelector('#year');
+const options = {  year: "numeric"};
+year.innerHTML = new Date().toLocaleDateString("en-US", options);
 
-document.getElementById(lastModified).textContent = new Date().toLocaleDateString("en-US",options); 
+let oLastModif = new Date(document.lastModified);
+const options2 = {  month: "long",
+day: "numeric",
+year: "numeric",
+hour: "numeric"};
+lastModified.innerHTML = new Date().toLocaleDateString("en-US", options2);
