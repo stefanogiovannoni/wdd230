ae7dd957eef75d0e26f1902eabdd264ea0ecfd31
Stefano Giovanoni <stefanogiovannonii@gmail.com>
2023-11-13 12:22:01 -0300

    activity group

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -9,3 +9,27 @@ year: "numeric",
 hour: "numeric"};
 lastModified.innerHTML = new Date().toLocaleDateString("en-US", options2);
 
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
