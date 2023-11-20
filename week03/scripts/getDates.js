75641edfb2c80559f89cc960d50b232200d4d5e5
Stefano Giovanoni <stefanogiovannonii@gmail.com>
2023-11-20 12:40:28 -0300

    lazy load

---
+++ week03/scripts/getDates.js
@@ -0,0 +1,10 @@
+const year = document.querySelector('#year');
+const options = {  year: "numeric"};
+year.innerHTML = new Date().toLocaleDateString("en-US", options);
+
+let oLastModif = new Date(document.lastModified);
+const options2 = {  month: "long",
+day: "numeric",
+year: "numeric",
+hour: "numeric"};
+lastModified.innerHTML = new Date().toLocaleDateString("en-US", options2);
