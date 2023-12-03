6b9ae36d51f9ec41246ee545426155a259882417
Stefano Giovanoni <stefanogiovannonii@gmail.com>
2023-12-02 23:35:51 -0300

    json links

--- scripts/links.js
+++ scripts/links.js
@@ -2,22 +2,25 @@ const baseURL = "https://stefanogiovannoni.github.io/wdd230/";
 
 const linksURL = "https://stefanogiovannoni.github.io/wdd230/data/links.json";
 
+const ul = document.querySelector(".homework");
+
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.weeks);
 }
 
 getLinks();
 
 function displayLinks(weeks) {
-    weeks.forEach(weekindex => {
+    weeks.forEach(week => {
         const li = document.createElement("li");
-        li.innerHTML = `${weekindex.week}:`
-        weekindex.links.forEach(link => {
-            li.innerHTML + li.innerHTML `<a href="${link.url}">${link.title}</a> |`
+        li.innerHTML = `${week.week}:`
+        week.links.forEach(links => {
+            li.innerHTML += `<a href="${links.url}">${links.title}</a> |`
         })
-        li.innerHTML = li.innerHTML(0, -1);
+        li.innerHTML = li.innerHTML.slice(0, -1);
         ul.appendChild(li);
     });
 }
+
